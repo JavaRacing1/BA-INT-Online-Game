62032b2818cc7dad8d5e0264fd954c665a34128d
JavaRacing <61991037+JavaRacing1@users.noreply.github.com>
2024-02-07 08:07:27 +0100

    Fix intendation

--- script/ui/screen/SettingsWindow.cs
+++ script/ui/screen/SettingsWindow.cs
@@ -2,26 +2,26 @@ using Godot;
 
 namespace OnlineGame
 {
-	/// <summary>
-	/// Screen for the user to change his settings
-	/// </summary>
-	public partial class SettingsWindow : CanvasLayer
-	{
-		private void OnCancelButtonPressed()
-		{
-			Visible = false;
-		}
+    /// <summary>
+    /// Screen for the user to change his settings
+    /// </summary>
+    public partial class SettingsWindow : CanvasLayer
+    {
+        private void OnCancelButtonPressed()
+        {
+            Visible = false;
+        }
 
-		private void OnDiscardButtonPressed()
-		{
-			//TODO: Reset unsaved changes
-		}
+        private void OnDiscardButtonPressed()
+        {
+            //TODO: Reset unsaved changes
+        }
 
-		private void OnApplyButtonPressed()
-		{
-			//TODO: Apply changes
-			Visible = false;
-		}
-	}
+        private void OnApplyButtonPressed()
+        {
+            //TODO: Apply changes
+            Visible = false;
+        }
+    }
 }
 
